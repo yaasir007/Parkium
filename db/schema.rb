d1fb6050b1b9194cd5f23819e6183977a9806588
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-15 18:41:35 +0400

    created review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_143854) do
+ActiveRecord::Schema.define(version: 2021_11_15_144101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,17 @@ ActiveRecord::Schema.define(version: 2021_11_15_143854) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.bigint "parking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["parking_id"], name: "index_reviews_on_parking_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "slots", force: :cascade do |t|
     t.integer "slot_number"
     t.bigint "parking_id", null: false
@@ -63,5 +74,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_143854) do
   add_foreign_key "bookings", "parkings"
   add_foreign_key "bookings", "slots"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "parkings"
+  add_foreign_key "reviews", "users"
   add_foreign_key "slots", "parkings"
 end
