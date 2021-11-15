1cdcca6fd87cd16a4425c170d1bea0dd75dbc525
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-15 23:45:51 +0400

    added cloudinary

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -2,4 +2,6 @@ class Parking < ApplicationRecord
     has_many :slots, dependent: :destroy
     has_many :bookings, dependent: :destroy
     has_many :reviews, dependent: :destroy
+    has_one_attached :photo
+
 end
