0208ced275d0c11a37916b8aff2170d5dbfaf39c
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-16 00:28:55 +0400

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :parkings
+  resources :slots
+  resources :reviews
+
+  end
+
 end
