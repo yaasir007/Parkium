2ae90f82c0457862e2ef3ca8721a87db80666e28
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-15 20:25:55 +0400

    added firstname, lastname, phone number and is_admin to users

---
+++ db/migrate/20211115162229_add_first_name_to_users.rb
@@ -0,0 +1,8 @@
+class AddFirstNameToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone_number, :integer
+    add_column :users, :is_admin, :boolean, default: :false
+  end
+end
