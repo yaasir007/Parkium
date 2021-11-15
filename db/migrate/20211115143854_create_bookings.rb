c5d00a0223f2dfc11d338e6a059b372293c78755
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-15 18:39:42 +0400

    Created booking model

---
+++ db/migrate/20211115143854_create_bookings.rb
@@ -0,0 +1,14 @@
+class CreateBookings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bookings do |t|
+      t.date :date
+      t.time :start_time
+      t.time :end_time
+      t.references :user, null: false, foreign_key: true
+      t.references :slot, null: false, foreign_key: true
+      t.references :parking, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
