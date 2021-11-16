550677696119322b94d9ad60790993606493d4f1
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-16 17:13:12 +0400

    show completed

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,6 +1,11 @@
 class ParkingsController < ApplicationController
+    before_action :find_index, only: [:show, :edit, :update, :destroy]
+    
     def index
         @parkings = Parking.all
         @user = current_user
+    end
+
+    def show
     end
 end
