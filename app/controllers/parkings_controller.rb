aa1cf806bb46f34b7e7c13f2ebfb063253dd9e40
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-16 17:12:31 +0400

    index completed

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,21 +1,6 @@
 class ParkingsController < ApplicationController
-    
     def index
         @parkings = Parking.all
+        @user = current_user
     end
-
-    def show
-        @parking = Parking.find(params[:id])
-    end
-
-    def new
-        @parking = Parking.new
-    end
-
-    # def create
-    #     @parking = Parking.find(params_parking)
-    # end
-
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users 
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   resources :parkings
-  
+
   devise_scope :user do
     authenticated :user do
       root "parkings#index", as: :authenticated_root
