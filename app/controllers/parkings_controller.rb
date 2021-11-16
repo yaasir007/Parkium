0cd0a60b9213db308bbf6ec477556401854fc228
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-16 14:10:51 +0400

    index for admins

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -4,6 +4,18 @@ class ParkingsController < ApplicationController
         @parkings = Parking.all
     end
 
-    
+    def show
+        @parking = Parking.find(params[:id])
+    end
+
+    def new
+        @parking = Parking.new
+    end
+
+    # def create
+    #     @parking = Parking.find(params_parking)
+    # end
+
+
 
 end
