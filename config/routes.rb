f8641812f3d2822d7f8829fa0d2a68ab453997a7
AnasSoobratty <soobratty.as@gmail.com>
2021-11-17 14:05:47 +0400

    Booking Controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :parkings do
-  delete 'parkings/:id', to: 'parkings#destroy', as: :destroy
-
-  resources :reviews 
-  end
-
+    delete 'parkings/:id', to: 'parkings#destroy', as: :destroy
+    resources :reviews
+    resources :bookings, only:[:new, :create, :index, :show]
+end
+    resources :bookings, only:[:destroy, :edit, :update]
   devise_scope :user do
     authenticated :user do
       root "parkings#index", as: :authenticated_root
