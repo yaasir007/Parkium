51dbeff358378012463c3ff00645eb9e1b8635de
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-17 22:40:42 +0400

    remove the payable thing in the admin parking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -18,7 +18,9 @@ before_action :booking_params, only: :create
       @booking = Booking.new(booking_params)
       @booking.user_id = @user.id
       @booking.parking_id = @parking.id
+      @counter = 1
       if @booking.save!
+        @parking.available_slots
           redirect_to parking_bookings_path(@parking)
       else
           redirect_to new_parking_booking_path(@parking)
