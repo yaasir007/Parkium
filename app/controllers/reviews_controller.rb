e6794bd2230d6465f4a7db78ccf20b6e2867f7e2
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-17 12:13:32 +0400

    new completed for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,5 +5,19 @@ class ReviewsController < ApplicationController
         @reviews = Review.all
     end
 
+    def new
+        @user = current_user
+        @parking = Parking.find(params[:parking_id])
+        @review = Review.new
+    end
 
+   
+
+
+
+    private
+
+    def params_reviews
+        params.require(:reviews).permit(:content, :rating)
+    end
 end
