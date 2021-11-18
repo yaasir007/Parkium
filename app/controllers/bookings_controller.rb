c35dc65cc84b1744e56d25e6335fc86444b33f5c
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-18 12:30:46 +0400

    removed all heads from the code

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,6 @@
 class BookingsController < ApplicationController
 before_action :booking_params, only: :create
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  def index
-    @bookings = Booking.all
-    @user = current_user
-  end
-
-=======
->>>>>>> e38d7f6e73d2134f9ac984e757153186a653ec56
-=======
->>>>>>> e38d7f6e73d2134f9ac984e757153186a653ec56
   def new
     @user = current_user
     @parking = Parking.find(params[:parking_id])
@@ -24,22 +13,9 @@ before_action :booking_params, only: :create
       @booking = Booking.new(booking_params)
       @booking.user_id = @user.id
       @booking.parking_id = @parking.id
-<<<<<<< HEAD
-<<<<<<< HEAD
-      @counter = 1
-      if @booking.save!
-        @parking.available_slots
-          redirect_to parking_bookings_path(@parking)
-=======
       if @booking.save!
           flash[:alert] = "Booking Created."
           redirect_to mybookings_path
->>>>>>> e38d7f6e73d2134f9ac984e757153186a653ec56
-=======
-      if @booking.save!
-          flash[:alert] = "Booking Created."
-          redirect_to mybookings_path
->>>>>>> e38d7f6e73d2134f9ac984e757153186a653ec56
       else
           redirect_to new_parking_booking_path(@parking)
       end
