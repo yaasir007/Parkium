07de3c32d98547f9ce76d0fe264a8a2ecfb815d5
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-18 15:42:19 +0400

    seeds completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,21 +10,21 @@ Parking.destroy_all
 Booking.destroy_all
 Review.destroy_all
 
-Parking.create(name: "CarPark " , address: "Port-Louis" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 10  ,available_slots: 10 ,user_id: User.last )
+Parking.create(name: "CarPark 1" , address: "Port-Louis" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 10  ,available_slots: 10 ,user_id: User.last )
 
-Parking.create(name: "CarPark " , address: "Rose-Hill" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 20  ,available_slots: 10 ,user_id: User.last )
+Parking.create(name: "CarPark 2" , address: "Rose-Hill" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 20  ,available_slots: 10 ,user_id: User.last )
 
-Parking.create(name: "CarPark " , address: "Grand-Port" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 30  ,available_slots: 10 ,user_id: User.last )
+Parking.create(name: "CarPark 3" , address: "Grand-Port" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 30  ,available_slots: 10 ,user_id: User.last )
 
-Parking.create(name: "CarPark " , address: "Grand Bay" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 40  ,available_slots: 10 ,user_id: User.last )
+Parking.create(name: "CarPark 4" , address: "Grand Bay" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 40  ,available_slots: 10 ,user_id: User.last )
 
-Parking.create(name: "CarPark " , address: "Rose-Belle", description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 50  ,available_slots: 10 ,user_id: User.last )
+Parking.create(name: "CarPark 5" , address: "Rose-Belle", description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 50  ,available_slots: 10 ,user_id: User.last )
 
-Parking.create(name: "CarPark " , address: "Belle-Rose" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 60  ,available_slots: 10 ,user_id: User.last )
+Parking.create(name: "CarPark 6" , address: "Belle-Rose" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 60  ,available_slots: 10 ,user_id: User.last )
 
-Parking.create(name: "CarPark " , address: "Ebene" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 70  ,available_slots: 10 ,user_id: User.last )
+Parking.create(name: "CarPark 7" , address: "Ebene" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 70  ,available_slots: 10 ,user_id: User.last )
 
-Parking.create(name: "CarPark " , address: "Phoniex", description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 80  ,available_slots: 10 ,user_id: User.last )
+Parking.create(name: "CarPark 8" , address: "Phoniex", description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 80  ,available_slots: 10 ,user_id: User.last )
 
 
 
