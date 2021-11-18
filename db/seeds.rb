707184e4e59a37e8a87b1a5aed6dc6cc115d0be6
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-18 13:44:50 +0400

    Seeds completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Parking.destroy_all
+Booking.destroy_all
+Review.destroy_all
+
+Parking.create(name: "CarPark " , address: "Port-Louis" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 10  ,available_slots: 10 ,user_id: User.last )
+
+Parking.create(name: "CarPark " , address: "Rose-Hill" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 20  ,available_slots: 10 ,user_id: User.last )
+
+Parking.create(name: "CarPark " , address: "Grand-Port" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 30  ,available_slots: 10 ,user_id: User.last )
+
+Parking.create(name: "CarPark " , address: "Grand Bay" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 40  ,available_slots: 10 ,user_id: User.last )
+
+Parking.create(name: "CarPark " , address: "Rose-Belle", description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 50  ,available_slots: 10 ,user_id: User.last )
+
+Parking.create(name: "CarPark " , address: "Belle-Rose" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 60  ,available_slots: 10 ,user_id: User.last )
+
+Parking.create(name: "CarPark " , address: "Ebene" , description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 70  ,available_slots: 10 ,user_id: User.last )
+
+Parking.create(name: "CarPark " , address: "Phoniex", description: "Fully secure Parking Area with Cameras and Guards" , number_of_slots: 10 , fees: 80  ,available_slots: 10 ,user_id: User.last )
+
+
+
