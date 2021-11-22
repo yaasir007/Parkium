1b03acbcac6a0ca65afd05191ad9dfd7f581a8ce
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-22 08:42:44 +0400

    added javascript and push mapbox key to heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,8 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
+import { initMapbox } from '../plugins/init_mapbox';
+
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -27,4 +29,5 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initMapbox();
 });
