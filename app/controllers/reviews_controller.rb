71ca60219cb305ca114bc653bacb3f5115892665
AnasSoobratty <soobratty.as@gmail.com>
2021-11-22 19:51:13 +0400

    Added Review Features to parking show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,46 +1,40 @@
 class ReviewsController < ApplicationController
-before_action :params_reviews, only: [:create, :edit]
-before_action :find_user, only:[:index, :new, :create, :destroy]
+  before_action :params_reviews, only: [:create] # :edit No need for edit, not a feature yet
+  before_action :find_user # only:[:new, :create, :destroy]///No need to specify.Only those 3 method exist
 
-    def index
-        @parking = Parking.find(params[:parking_id])
-        @reviews = Review.all
+  def new
+    @parking = Parking.find(params[:parking_id])
+    @review = Review.new
+  end
+
+  def create
+    @parking = Parking.find(params[:parking_id])
+    @review = Review.new(params_reviews)
+    @review.user_id = @user.id
+    @review.parking_id = @parking.id
+    if @review.save!
+      redirect_to parking_path(@parking)
+      flash[:alert] = "Review Created."
+    else
+      redirect_to new_parking_review_path(@parking)
     end
+  end
 
-    def new
+  def destroy
+    @parking = Parking.find(params[:parking_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to parking_path(@parking)
+    flash[:alert] = "Review Removed."
+  end
 
-        @parking = Parking.find(params[:parking_id])
-        @review = Review.new
-    end
+  private
 
-    def create
-        @parking = Parking.find(params[:parking_id])
-        @review = Review.new(params_reviews)
-        @review.user_id = @user.id
-        @review.parking_id = @parking.id
-        if @review.save!
-            redirect_to parking_reviews_path(@parking)
-            flash[:alert] = "Review Created."
-        else
-            redirect_to new_parking_review_path(@parking)
-        end    
-    end
+  def find_user
+    @user = current_user
+  end
 
-   def destroy
-        @parking = Parking.find(params[:parking_id])
-        @review = Review.find(params[:id])
-        @review.destroy
-        redirect_to parking_reviews_path(@parking)
-        flash[:alert] = "Review Removed."
-   end
-       
-    private
-
-    def find_user
-        @user = current_user
-    end
-
-    def params_reviews
-        params.require(:review).permit(:content, :rating)
-    end
+  def params_reviews
+    params.require(:review).permit(:content, :rating)
+  end
 end
