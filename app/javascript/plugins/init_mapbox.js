1da90b016c94c5b8a061e549c7772ad3c61f6b00
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-24 20:54:07 +0400

    added , in controller parking

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -13,10 +13,20 @@ const initMapbox = () => {
         style: 'mapbox://styles/yaasir007/ckwbwxcy01wxg15qoishglzuj',
         });
 
+
     const markers = JSON.parse(mapElement.dataset.markers);
         markers.forEach((marker) => {
             const popup = new mapboxgl.Popup().setHTML(marker.info_window);
-                new mapboxgl.Marker()
+
+             // Create a HTML element for your custom marker
+            const element = document.createElement('div');
+            element.className = 'marker';
+            element.style.backgroundImage = `url('${marker.image_url}')`;
+            element.style.backgroundSize = 'contain';
+            element.style.width = '25px';
+            element.style.height = '25px';
+
+                new mapboxgl.Marker(element)
                 .setLngLat([ marker.lng, marker.lat ])
                 .setPopup(popup)
                 .addTo(map);
