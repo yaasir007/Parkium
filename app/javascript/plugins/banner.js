db1f73647c088d3fe19ecf9d48b1f8730d993e46
AnasSoobratty <soobratty.as@gmail.com>
2021-11-25 12:08:38 +0400

    added styles

--- app/javascript/plugins/banner.js
+++ app/javascript/plugins/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["Ease your life", "Book a parking...", "And be on the go!"],
-    typeSpeed: 50,
+    typeSpeed: 70,
     loop: true
   });
 }
